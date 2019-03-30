5314b48eb283c486a00e7b196819bc1a3328a151
jzubak <jzubak81@gmail.com>
2019-03-30 12:27:05 -0400

    document clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -750,12 +750,12 @@ $(document).ready(function () {
     });
 });
 // Initialize Firebase
-                var config = {
-                    apiKey: "AIzaSyAwhvcz5UIY3y0nZaA76lSHEm24P99-Wzg",
-                    authDomain: "project1-2f7ae.firebaseapp.com",
-                    databaseURL: "https://project1-2f7ae.firebaseio.com",
-                    projectId: "project1-2f7ae",
-                    storageBucket: "project1-2f7ae.appspot.com",
-                    messagingSenderId: "842716287063"
-                };
-                firebase.initializeApp(config);
+                // var config = {
+                //     apiKey: "AIzaSyAwhvcz5UIY3y0nZaA76lSHEm24P99-Wzg",
+                //     authDomain: "project1-2f7ae.firebaseapp.com",
+                //     databaseURL: "https://project1-2f7ae.firebaseio.com",
+                //     projectId: "project1-2f7ae",
+                //     storageBucket: "project1-2f7ae.appspot.com",
+                //     messagingSenderId: "842716287063"
+                // };
+                // firebase.initializeApp(config);
