98a6a3b2c29b03e03cb915ea9bc23d4033e4eb56
Rachael <rdougherty031@gmail.com>
2019-04-01 19:22:10 -0400

    firebase added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1205,16 +1205,45 @@ $(document).ready(function () {
                 };
             });
         });
-                    // Initialize Firebase
-                    // var config = {
-                    //     apiKey: "AIzaSyAwhvcz5UIY3y0nZaA76lSHEm24P99-Wzg",
-                    //     authDomain: "project1-2f7ae.firebaseapp.com",
-                    //     databaseURL: "https://project1-2f7ae.firebaseio.com",
-                    //     projectId: "project1-2f7ae",
-                    //     storageBucket: "project1-2f7ae.appspot.com",
-                    //     messagingSenderId: "842716287063"
-                    // };
-                    // firebase.initializeApp(config);
-    });
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAwhvcz5UIY3y0nZaA76lSHEm24P99-Wzg",
+            authDomain: "project1-2f7ae.firebaseapp.com",
+            databaseURL: "https://project1-2f7ae.firebaseio.com",
+            projectId: "project1-2f7ae",
+            storageBucket: "project1-2f7ae.appspot.com",
+            messagingSenderId: "842716287063"
+        };
+        firebase.initializeApp(config);
+        var database = firebase.database();
 
-});
+
+            // Grabs user input
+            var city1 = $("#startLocation").val();
+            var city2 = $("#endLocation").val();
+
+            var newCity1 = {
+                city1name: city1,
+                city2name: city2,
+            };
+            database.ref().push(newCity1);
+            // database.ref().push(newCity2);
+            console.log(newCity.city1name);
+            console.log(newCity.city2name);
+        // });
+        database.ref().on("child_added", function (childSnaphot) {
+            console.log(childSnaphot.val());
+
+            var cityname1 = childSnaphot.val().city1name;
+            var cityname2 = childSnaphot.val().city2name;
+
+            var newRow = $("<tr>").append(
+                $("<td>").text(cityname1),
+                $("<td>").text(cityname2),
+            );
+            $("#search-table > tbody").append(newRow);
+        });
+    });
+    });
+// });
+// });
